c30eae60c4e63cb4f0c371ec30670c7f63f3914c
bogdan-svg <58813486+bogdan-svg@users.noreply.github.com>
2021-01-05 16:33:28 +0200

    zamina metodiv

---
+++ Calculator/Actions.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Actions
+    {
+        double result;
+        double numbersecond;
+        double rezultend;
+        char sign;
+        public void plus(double numberfirst, char sign)
+        {            
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {sign} number 2");
+            numbersecond = Convert.ToDouble(Console.ReadLine());
+            result = numberfirst + numbersecond;
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {sign} {numbersecond} = and or + - * /");
+            sign = Convert.ToChar(Console.ReadLine());
+            while (true)
+            {
+                
+                if (sign == '=')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{numberfirst} {sign} {numbersecond} = {result}");
+                    break;
+                }
+                else if (sign == '+')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result += c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '-')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result -= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} - {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '*')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result *= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} * {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '/')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result /= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} / {c} = {result}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("lox");
+                    Console.ReadKey();
+                }
+            }
+        }
+        public void subtraction(double numberfirst, char z)
+        {            
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} number 2");
+            numbersecond = Convert.ToDouble(Console.ReadLine());
+            result = numberfirst - numbersecond;
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} {numbersecond} = and or + - * /");
+            sign = Convert.ToChar(Console.ReadLine());
+
+
+            while (true)
+            {
+                if (sign == '=')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{numberfirst} {z} {numbersecond} = {result}");
+                    break;
+                }
+                else if (sign == '+')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result += c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '-')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result -= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '*')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result *= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '/')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result /= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("lox");
+                    Console.ReadKey();
+                }
+            }
+        }
+        public void multiplication(double numberfirst, char z)
+        {            
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} number 2");
+            numbersecond = Convert.ToDouble(Console.ReadLine());
+            result = numberfirst * numbersecond;
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} {numbersecond} = and or + - * /");
+            sign = Convert.ToChar(Console.ReadLine());
+
+            while (true)
+            {
+                if (sign == '=')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{numberfirst} {z} {numbersecond} = {result}");
+                    break;
+                }
+                else if (sign == '+')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result += c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '-')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result -= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '*')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result *= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '/')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result /= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("lox");
+                    Console.ReadKey();
+                }
+            }
+        }
+        public void division(double numberfirst, char z)
+        {            
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} number 2");
+            numbersecond = Convert.ToDouble(Console.ReadLine());
+            result = numberfirst - numbersecond;
+            Console.Clear();
+            Console.WriteLine($"{numberfirst} {z} {numbersecond} = and or + - * /");
+            sign = Convert.ToChar(Console.ReadLine());
+
+            while (true)
+            {
+                if (sign == '=')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{numberfirst} {z} {numbersecond} = {result}");
+                    break;
+                }
+                else if (sign == '+')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result += c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '-')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result -= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '*')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result *= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else if (sign == '/')
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{result} {sign} number");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    rezultend = result;
+                    result /= c;
+                    Console.Clear();
+                    Console.WriteLine($"{rezultend} {sign} {c} = and");
+                    sign = Convert.ToChar(Console.ReadLine());
+                    if (sign == '=')
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{rezultend} + {c} = {result}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("lox");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actions ac = new Actions(); 
+            double numberfirst;
+            char sign;
+
+
+
+            while (true)
+            {
+                Console.WriteLine("number");
+                if (double.TryParse(Console.ReadLine(), out numberfirst))
+                {                    
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{numberfirst} sign");
+                        if (char.TryParse(Console.ReadLine(), out sign))
+                        {
+                            if (sign == '+')
+                            {
+                                ac.plus(numberfirst, sign);
+                                Console.WriteLine("exit? esc");
+                                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                                {
+                                    Process.GetCurrentProcess().Kill();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    break;
+                                }                                
+                            }
+                            else if (sign == '-')
+                            {
+                                ac.subtraction(numberfirst, sign);
+                                Console.WriteLine("exit? esc");
+                                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                                {
+                                    Process.GetCurrentProcess().Kill();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    break;
+                                }
+                            }
+                            else if (sign == '*')
+                            {
+                                ac.multiplication(numberfirst, sign);
+                                Console.WriteLine("exit? esc");
+                                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                                {
+                                    Process.GetCurrentProcess().Kill();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    break;
+                                }
+                            }
+                            else if (sign == '/')
+                            {
+                                ac.division(numberfirst, sign);
+                                Console.WriteLine("exit? esc");
+                                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                                {
+                                    Process.GetCurrentProcess().Kill();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("nemae takogo znaky");
+                                Console.WriteLine("exit? esc");
+                                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                                {
+                                    Process.GetCurrentProcess().Kill();
+                                }
+                                else
+                                    Console.Clear();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("exit? esc");
+                            if (Console.ReadKey().Key == ConsoleKey.Escape)
+                            {
+                                Process.GetCurrentProcess().Kill();
+                            }
+                            else
+                                Console.Clear();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("exit? esc");
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    {
+                        Process.GetCurrentProcess().Kill();
+                    }
+                    else
+                        Console.Clear();
+                }
+            }
+
+
+        }
+    }
+}
